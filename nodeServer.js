3a418234fd7955d38df8aca81c1c57893f75290e
King Elisha <ek.chibueze@gmail.com>
2019-09-08 07:30:00 +0100

    serve other static files with urls pointing to the server

--- nodeServer.js
+++ nodeServer.js
@@ -53,4 +53,34 @@ const fs = require('fs'); //? gives access to servers(this computers) file syste
 
 // server.listen(3000);
 
+//? this server is configured to serve static files
+const server = http.createServer((req, res) => {
+  console.log("request: " + req.url);
+  if(req.url == "/"){
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
+    const homePageHTML = fs.readFileSync('node.html');
+    res.write(homePageHTML);
+  } else if(req.url.indexOf('/img/node.svg') != -1){
+    res.writeHead(200, {
+      'content-type': 'image/svg+xml'
+    });
+    const img = fs.readFileSync("img/node.svg");
+    res.write(img);
+  } else if(req.url.indexOf('/css/style.css') != -1){
+    res.writeHead(200, {
+      'content-type': 'text/css'
+    });
+    const css = fs.readFileSync("css/style.css");
+    res.write(css);
+  } else{
+    res.writeHead(404, {
+      'content-type': 'image/*'
+    });
+    res.write("<h1>Page Not Found");
+  }
+  res.end();
+});
 
+server.listen(3000);
