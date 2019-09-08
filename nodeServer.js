45e38a2a1e6fb10096a9f1174c9e7148228aba07
King Elisha <ek.chibueze@gmail.com>
2019-09-08 06:47:07 +0100

    basic routing

--- nodeServer.js
+++ nodeServer.js
@@ -1,15 +1,34 @@
 const http = require('http');
 
+// //? this server responds to any url on localhost:3000
+// const server = http.createServer((req, res) => {
+//   //? add headers to response
+//   res.writeHead(200, {
+//     'content-type': 'text/html'
+//   });
+//   //? add body to response
+//   res.write("<h1>Hello World");
+//   //? terminate http connection
+//   res.end();
+// });
+
+// //? listen for traffic on port 3000
+// server.listen(3000);
+
+//? this server is configured to serve the home page only to the "/" url
 const server = http.createServer((req, res) => {
-  // add headers to response
-  res.writeHead(200, {
-    'content-type': 'text/html'
-  });
-  // add body to response
-  res.write("<h1>Hello World");
-  // terminate http connection
+  if(req.url == "/"){
+    res.writeHead(200, {
+      'content-type': 'text/html'
+    });
+    res.write("<h1>Hello World");
+  } else{
+    res.writeHead(404, {
+      'content-type': 'text/html'
+    });
+    res.write("<h1>Page Not Found");
+  }
   res.end();
 });
 
-// listen for traffic on port 3000
 server.listen(3000);
