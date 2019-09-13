f3d3aac07c066a5c4d303ebf754c61ecbbc3c9f1
King Elisha <ek.chibueze@gmail.com>
2019-09-13 12:52:49 +0100

    added handlebars templating engine

--- rendering.js
+++ rendering.js
@@ -15,9 +15,15 @@ server.use(express.static('public'));
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
 
-server.set("view engine", "ejs");
+server.set("view engine", "hbs");
 server.set("views", path.join(__dirname, "views"));
 
-server.get('/', (req, res, next) => {
-  res.render("index");
+server.get('/ejs', (req, res, next) => {
+  if(server.get("view engine") == "ejs") res.render("ejs/index");
+  else res.send(`<p>EJS is not the templating engine<br>Use <strong>${server.get("view engine")}</strong></p>`);
+});
+
+server.get('/hbs', (req, res, next) => {
+  if(server.get("view engine") == "hbs") res.render("hbs/index");
+  else res.send(`<h1>HBS is not the templating engine<br>Use <strong>${server.get("view engine")}</strong></h1>`);
 });
