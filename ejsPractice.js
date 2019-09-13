c3b0ac53bf431afd19d826839a05ada94eadff60
King Elisha <ek.chibueze@gmail.com>
2019-09-13 13:08:59 +0100

    ejs focus

---
+++ ejsPractice.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+const helmet = require("helmet");
+
+const server = express();
+
+server.listen(3000);
+
+// basic security
+server.use(helmet());
+
+// serve up static files
+server.use(express.static('public'));
+// parse json and urlencoded data into req.body
+server.use(express.json());
+server.use(express.urlencoded({extended: true}));
+
+server.set("view engine", "ejs");
+server.set("views", path.join(__dirname, "views", "ejs"));
+
+server.get('/', (req, res, next) => {
+  res.render("index");
+});
